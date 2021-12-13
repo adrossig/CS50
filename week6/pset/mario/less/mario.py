c06aaf3b9e93c6fc4024bf60c5001ab6b7e51a47
Adrien <adrienrossignol1@gmail.com>
2021-12-13 16:50:13 +0100

    add mario

---
+++ week6/pset/mario/less/mario.py
@@ -0,0 +1,30 @@
+from cs50 import get_int
+
+
+def numbers():
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        block(height)
+    else:
+        numbers()
+
+
+def print_space(number):
+    for i in range(number):
+        print(" ", end="")
+
+
+def print_block(number):
+    for i in range(number):
+        print("#", end="")
+
+
+def block(height):
+    for i in range(height):
+        print_space((height - i - 1))
+        print_block((i + 1))
+        print("")
+
+
+numbers()
+
